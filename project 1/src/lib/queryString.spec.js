1c03a8f7cf12d03d365d4004fefea5a1dee4fa8b
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-03-16 23:31:05 -0300

    convert qs to object and separeted values

--- project 1/src/lib/queryString.spec.js
+++ project 1/src/lib/queryString.spec.js
@@ -48,4 +48,13 @@ describe('Query string to object', () => {
       name: 'Gabriel',
     });
   });
+
+  it('should convert a query string to an object taking care of comma separeted values', () => {
+    const qs = 'name=Gabriel&abilitites=JS,VUE,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Gabriel',
+      abilitites: ['JS', 'VUE', 'TDD'],
+    });
+  });
 });
